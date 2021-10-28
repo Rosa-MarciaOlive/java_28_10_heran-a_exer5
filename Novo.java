f55023615315838dcb9b01c81dd34df510cf3d04
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2021-10-28 17:50:33 +0000

    Novo finalizado.

--- Novo.java
+++ Novo.java
@@ -1,1 +1,13 @@
-public
+public class Novo extends Imovel{
+
+//atributo
+    private double adicionalPreço;
+
+    public void setAdicionalPreço(double adicionalPreço){
+        this.adicionalPreço=adicionalPreço;
+    }
+    public double getAdicionalPreço(){
+        return this.adicionalPreço;
+    }
+
+}
