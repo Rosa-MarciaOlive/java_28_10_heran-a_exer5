94bd0747d167a7aae27431718a2b48df5074ff7e
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2021-10-28 17:38:29 +0000

    Imovel finalizado.

---
+++ Imovel.java
@@ -0,0 +1,23 @@
+//classe de modelagem
+public class Imovel{
+
+//atributos de classe-->endereço e preço
+    private String endereço;
+    private double preço;
+
+//
+public String getEndereço(){
+    return this.endereço;
+}
+public void setEndereço(String endereço){
+    this.endereço=endereço;
+}
+
+public double getPreço(){
+    return this.preço;
+}
+public void setPreço(double preço){
+    this.preço=preço;
+}
+
+}
