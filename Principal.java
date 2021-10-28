f8a2a9ad53361fd135faae2ba0959dc8ebab7731
Rosa-MarciaOlive <rosa.marciaolive@gmail.com>
2021-10-28 19:25:27 +0000

    Velho.java

--- Principal.java
+++ Principal.java
@@ -9,6 +9,12 @@ public class Principal{
     novo.setPreco(250000);
     novo.setAdicionalPreco(0.2*novo.getPreco());
     System.out.println(novo.getPreco()+novo.getAdicionalPreco());
-    }
+
+//CONTINUAR............
+    // velho.setEndereco("Japão");
+    // velho.setPreco(130000);
+    // velho.setAdicionalPreco(0.1*velho.getPreco());
+    // System.out.println("Preço do imovel velho: R$ "+(velho.getPreço()- velho.getDescontoPreco()));
+     }
 
 }
